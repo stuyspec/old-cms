2341a1c78b149a0e39f47cf41d673357c0b86306
Jason Kao <jasonkao85@gmail.com>
2018-01-21 12:07:18 -0500

    d

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.1605' };
+let VersionInfo = { VERSION : '0.0.1606' };
 export default VersionInfo
