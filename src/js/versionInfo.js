e221f99c8ee9f23fa0895b18c17119501fc7856c
Darius Jankauskas <d5926j@gmail.com>
2018-02-05 21:41:17 -0500

    add sign-up link, fix `/sign_up`

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.1610' };
+let VersionInfo = { VERSION : '0.0.1665' };
 export default VersionInfo
